de8fa91b48495b152ea1d752574aa4d0298da38c
s-royy <sayanroy7009@gmail.com>
2026-04-21 22:03:27 +0530

    Implement sorting of students by marks
    Added a sorting function to sort students by their marks in descending order.

--- stl-sorting/sorting.cpp
+++ stl-sorting/sorting.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+struct Student {
+    string name;
+    int marks;
+};
+
+bool compare(Student a, Student b) {
+    return a.marks > b.marks;
+}
+
+int main() {
+    vector<Student> students = {
+        {"Amit", 85},
+        {"Riya", 92},
+        {"John", 78},
+        {"Sara", 88}
+    };
+
+    sort(students.begin(), students.end(), compare);
+
+    cout << "Students sorted by marks (descending):\n";
+    for (const auto& s : students) {
+        cout << s.name << " - " << s.marks << endl;
+    }
+
+    return 0;
+}
