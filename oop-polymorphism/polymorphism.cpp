f570f791809e72fd66f4de4d76ce5b4d053fadcb
s-royy <sayanroy7009@gmail.com>
2026-04-21 22:00:48 +0530

    Implement polymorphism with Shape, Rectangle, and Circle

--- oop-polymorphism/polymorphism.cpp
+++ oop-polymorphism/polymorphism.cpp
@@ -7,4 +7,33 @@ public:
     virtual ~Shape() {}
 };
 
+class Rectangle : public Shape {
+    double width, height;
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+    double area() override {
+        return width * height;
+    }
+};
 
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double r) : radius(r) {}
+    double area() override {
+        return 3.14159 * radius * radius;
+    }
+};
+
+int main() {
+    Shape* s1 = new Rectangle(4, 5);
+    Shape* s2 = new Circle(3);
+
+    cout << "Rectangle Area: " << s1->area() << endl;
+    cout << "Circle Area: " << s2->area() << endl;
+
+    delete s1;
+    delete s2;
+
+    return 0;
+}
